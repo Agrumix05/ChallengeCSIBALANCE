d90feae3ff525cd78119686739fc72f7a9ed633b
Agrumix05 <lditecco@outlook.com>
2025-12-19 17:28:35 +0100

    modified:   challengcsibalance/client/client_app.py 	modified:   challengcsibalance/server/server_app.py 	new file:   exploration/.~lock.submission.csv# 	modified:   exploration/detail.ipynb 	modified:   exploration/filtering.ipynb 	new file:   exploration/final_model.ipynb 	new file:   exploration/final_model.json 	new file:   exploration/submission.csv 	modified:   exploration/visualizzation.ipynb 	new file:   exploration/xgboost.ipynb 	new file:   final_model.json 	modified:   poetry.lock 	modified:   pyproject.toml

--- challengcsibalance/client/client_app.py
+++ challengcsibalance/client/client_app.py
@@ -1,80 +1,419 @@
-"""ChallengCSIBALANCE: A Flower / PyTorch app."""
+"""quickstart-xgboost: A Flower / XGBoost app."""
 
-import torch
+import warnings
+
+import numpy as np
+import xgboost as xgb
 from flwr.app import ArrayRecord, Context, Message, MetricRecord, RecordDict
 from flwr.clientapp import ClientApp
-
-from challengcsibalance.lib.task import Net, load_data
-from challengcsibalance.lib.task import test as test_fn
-from challengcsibalance.lib.task import train as train_fn
+from flwr.common.config import unflatten_dict
+from challengcsibalance.lib.task import load_data, clean_ts, convert_timeseries_columns, replace_keys
+warnings.filterwarnings("ignore", category=UserWarning)
+import pandas as pd
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_absolute_error, r2_score
 
 # Flower ClientApp
 app = ClientApp()
 
 
+def _local_boost(bst_input, num_local_round, train_dmatrix):
+    # Update trees based on local training data.
+    for i in range(num_local_round):
+        bst_input.update(train_dmatrix, bst_input.num_boosted_rounds())
+
+    # Bagging: extract the last N=num_local_round trees for sever aggregation
+    bst = bst_input[
+        bst_input.num_boosted_rounds()
+        - num_local_round : bst_input.num_boosted_rounds()
+    ]
+    return bst
+
+
 @app.train()
-def train(msg: Message, context: Context):
-    """Train the model on local data."""
-
-    # Load the model and initialize it with the received weights
-    model = Net()
-    model.load_state_dict(msg.content["arrays"].to_torch_state_dict())
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-
-    # Load the data
+def train(msg: Message, context: Context) -> Message:
+    
     partition_id = context.node_config["partition-id"]
     num_partitions = context.node_config["num-partitions"]
-    trainloader, _ = load_data(partition_id, num_partitions)
+    print('partiton ', partition_id, num_partitions)
 
-    # Call the training function
-    train_loss = train_fn(
-        model,
-        trainloader,
-        context.run_config["local-epochs"],
-        msg.content["config"]["lr"],
-        device,
+    
+    
+    groups_dfs = load_data(partition_id, num_partitions)
+    
+    for group, user_dfs in groups_dfs.items():
+        for user_id, df in user_dfs.items():
+            groups_dfs[group][user_id] = convert_timeseries_columns(df)
+    
+    for group, user_dfs in groups_dfs.items():
+        for user_id, df in user_dfs.items():
+            for col in df.columns:
+                if col.startswith('timeseries_'):
+                    df[col] = df[col].apply(lambda x: clean_ts(x))
+            groups_dfs[group][user_id] = df
+    
+
+    for group, users in groups_dfs.items():
+        for user_id, df in users.items():
+            df['deep_sleep_pct'] = np.nan
+            df['rem_sleep_pct'] = np.nan
+            df['light_sleep_pct'] = np.nan
+            df['awake_sleep_pct'] = np.nan
+            df['sleep_efficiency'] = np.nan
+            df['sleep_day_ptc'] = np.nan
+            for row_idx, row in df.iterrows():
+                sleep_deepSleepSeconds = row.get("sleep_deepSleepSeconds", np.nan)
+                sleep_lightSleepSeconds = row.get("sleep_lightSleepSeconds", np.nan)
+                sleep_remSleepSeconds = row.get("sleep_remSleepSeconds", np.nan)
+                sleep_timeSeconds = row.get("sleep_sleepTimeSeconds", np.nan)
+                sleep_awakeTimeSeconds = row.get("sleep_awakeSleepSeconds", np.nan)
+                
+
+
+                deep_pct = sleep_deepSleepSeconds / sleep_timeSeconds 
+                rem_pct = sleep_remSleepSeconds / sleep_timeSeconds
+                light_pct = sleep_lightSleepSeconds / sleep_timeSeconds
+                awake_pct = sleep_awakeTimeSeconds / sleep_timeSeconds
+                sleep_efficiency = sleep_timeSeconds / (sleep_timeSeconds + sleep_awakeTimeSeconds)
+                df.at[row_idx, 'deep_sleep_pct'] = deep_pct
+                df.at[row_idx, 'rem_sleep_pct'] = rem_pct
+                df.at[row_idx, 'light_sleep_pct'] = light_pct
+                df.at[row_idx, 'awake_sleep_pct'] = awake_pct
+                df.at[row_idx, 'sleep_efficiency'] = sleep_efficiency
+                df.at[row_idx, 'sleep_day_ptc'] = sleep_timeSeconds / 86400.0
+            
+            groups_dfs[group][user_id] = df
+    FEATURES = [
+        # sleep composition
+        "deep_sleep_pct",
+        "rem_sleep_pct",
+        "light_sleep_pct",
+        "awake_sleep_pct",
+        "sleep_efficiency",
+
+        # heart rate static
+        "hr_restingHeartRate",
+        "hr_lastSevenDaysAvgRestingHeartRate",
+        "hr_maxHeartRate",
+        "hr_minHeartRate",
+
+        # stress static
+        "str_avgStressLevel",
+        "str_maxStressLevel",
+
+        # activity
+        "act_totalCalories",
+        "act_activeKilocalories",
+        "act_distance",
+
+        # respiration static
+        "resp_lowestRespirationValue",
+        "resp_highestRespirationValue",
+        "resp_avgSleepRespirationValue",
+
+        'sleep_day_ptc'
+    ]
+
+    X_list = []
+    y_list = []
+
+    for group, users in groups_dfs.items():
+        for user_id, df in users.items():
+
+            df_model = df.copy()
+
+            # tieni solo righe valide
+            df_model = df_model.dropna(subset=["label"])
+
+            X = df_model[FEATURES]
+            y = df_model["label"]
+
+            X_list.append(X)
+            y_list.append(y)
+
+    X_all = pd.concat(X_list, axis=0)
+    y_all = pd.concat(y_list, axis=0)
+
+    X_all = X_all.replace([np.inf, -np.inf], np.nan)
+
+    split_idx = int(len(X_all) * 1)
+
+
+
+    X_train = X_all.iloc[:split_idx]
+    X_val   = X_all.iloc[split_idx:]
+
+
+
+    y_train = y_all.iloc[:split_idx]
+    y_val   = y_all.iloc[split_idx:]
+
+    model = XGBRegressor(
+        n_estimators=300,
+        max_depth=4,
+        learning_rate=0.05,
+        subsample=0.8,
+        
+        colsample_bytree=0.8,
+        reg_alpha=0.1,
+        reg_lambda=1.0,
+        random_state=42,
+        n_jobs=-1
     )
 
-    # Construct and return reply Message
-    model_record = ArrayRecord(model.state_dict())
+    # Training semplice
+
+    global_round = msg.content["config"]["server-round"]
+    if global_round == 1:
+        model.fit(
+            X_train,
+            y_train
+        )
+    else:
+        global_model = bytearray(msg.content["arrays"]["0"].numpy().tobytes())
+
+        # Load global model into booster
+        model.load_model(global_model)
+
+        # Local training
+        model.fit(
+            X_train,
+            y_train
+        )
+    # Save model   
+    booster = model.get_booster()
+    local_model = booster.save_raw("json")
+
+    model_np = np.frombuffer(local_model, dtype=np.uint8)
+    # Construct reply message
+    # Note: we store the model as the first item in a list into ArrayRecord,
+    # which can be accessed using index ["0"].
+    model_record = ArrayRecord([model_np])
     metrics = {
-        "train_loss": train_loss,
-        "num-examples": len(trainloader.dataset),
+        "num-examples": len(X_train),
     }
     metric_record = MetricRecord(metrics)
+
     content = RecordDict({"arrays": model_record, "metrics": metric_record})
+
+
     return Message(content=content, reply_to=msg)
 
 
+
+    # # Load model and data
+    # partition_id = context.node_config["partition-id"]
+    # num_partitions = context.node_config["num-partitions"]
+    # train_dmatrix, _, num_train, _ = load_data(partition_id, num_partitions)
+
+    # # Read from run config
+    # num_local_round = context.run_config["local-epochs"]
+    # # Flatted config dict and replace "-" with "_"
+    # cfg = replace_keys(unflatten_dict(context.run_config))
+    # params = cfg["params"]
+
+    # global_round = msg.content["config"]["server-round"]
+    # if global_round == 1:
+    #     # First round local training
+    #     bst = xgb.train(
+    #         params,
+    #         train_dmatrix,
+    #         num_boost_round=num_local_round,
+    #     )
+    # else:
+    #     bst = xgb.Booster(params=params)
+    #     global_model = bytearray(msg.content["arrays"]["0"].numpy().tobytes())
+
+    #     # Load global model into booster
+    #     bst.load_model(global_model)
+
+    #     # Local training
+    #     bst = _local_boost(bst, num_local_round, train_dmatrix)
+
+    # # Save model
+    # local_model = bst.save_raw("json")
+    # model_np = np.frombuffer(local_model, dtype=np.uint8)
+
+    # # Construct reply message
+    # # Note: we store the model as the first item in a list into ArrayRecord,
+    # # which can be accessed using index ["0"].
+    # model_record = ArrayRecord([model_np])
+    # metrics = {
+    #     "num-examples": num_train,
+    # }
+    # metric_record = MetricRecord(metrics)
+    # content = RecordDict({"arrays": model_record, "metrics": metric_record})
+    # return Message(content=content, reply_to=msg)
+
+
 @app.evaluate()
-def evaluate(msg: Message, context: Context):
-    """Evaluate the model on local data."""
+def evaluate(msg: Message, context: Context) -> Message:
 
-    # Load the model and initialize it with the received weights
-    model = Net()
-    model.load_state_dict(msg.content["arrays"].to_torch_state_dict())
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model.to(device)
 
-    # Load the data
     partition_id = context.node_config["partition-id"]
     num_partitions = context.node_config["num-partitions"]
-    _, valloader = load_data(partition_id, num_partitions)
+    groups_dfs = load_data(partition_id, num_partitions)
+    
+    for group, user_dfs in groups_dfs.items():
+        for user_id, df in user_dfs.items():
+            groups_dfs[group][user_id] = convert_timeseries_columns(df)
+    
+    for group, user_dfs in groups_dfs.items():
+        for user_id, df in user_dfs.items():
+            for col in df.columns:
+                if col.startswith('timeseries_'):
+                    df[col] = df[col].apply(lambda x: clean_ts(x))
+            groups_dfs[group][user_id] = df
+    
 
-    # Call the evaluation function
-    eval_loss, eval_acc = test_fn(
-        model,
-        valloader,
-        device,
+    for group, users in groups_dfs.items():
+        for user_id, df in users.items():
+            df['deep_sleep_pct'] = np.nan
+            df['rem_sleep_pct'] = np.nan
+            df['light_sleep_pct'] = np.nan
+            df['awake_sleep_pct'] = np.nan
+            df['sleep_efficiency'] = np.nan
+            df['sleep_day_ptc'] = np.nan
+            for row_idx, row in df.iterrows():
+                sleep_deepSleepSeconds = row.get("sleep_deepSleepSeconds", np.nan)
+                sleep_lightSleepSeconds = row.get("sleep_lightSleepSeconds", np.nan)
+                sleep_remSleepSeconds = row.get("sleep_remSleepSeconds", np.nan)
+                sleep_timeSeconds = row.get("sleep_sleepTimeSeconds", np.nan)
+                sleep_awakeTimeSeconds = row.get("sleep_awakeSleepSeconds", np.nan)
+                
+
+
+                deep_pct = sleep_deepSleepSeconds / sleep_timeSeconds 
+                rem_pct = sleep_remSleepSeconds / sleep_timeSeconds
+                light_pct = sleep_lightSleepSeconds / sleep_timeSeconds
+                awake_pct = sleep_awakeTimeSeconds / sleep_timeSeconds
+                sleep_efficiency = sleep_timeSeconds / (sleep_timeSeconds + sleep_awakeTimeSeconds)
+                df.at[row_idx, 'deep_sleep_pct'] = deep_pct
+                df.at[row_idx, 'rem_sleep_pct'] = rem_pct
+                df.at[row_idx, 'light_sleep_pct'] = light_pct
+                df.at[row_idx, 'awake_sleep_pct'] = awake_pct
+                df.at[row_idx, 'sleep_efficiency'] = sleep_efficiency
+                df.at[row_idx, 'sleep_day_ptc'] = sleep_timeSeconds / 86400.0
+            
+            groups_dfs[group][user_id] = df
+    FEATURES = [
+        # sleep composition
+        "deep_sleep_pct",
+        "rem_sleep_pct",
+        "light_sleep_pct",
+        "awake_sleep_pct",
+        "sleep_efficiency",
+
+        # heart rate static
+        "hr_restingHeartRate",
+        "hr_lastSevenDaysAvgRestingHeartRate",
+        "hr_maxHeartRate",
+        "hr_minHeartRate",
+
+        # stress static
+        "str_avgStressLevel",
+        "str_maxStressLevel",
+
+        # activity
+        "act_totalCalories",
+        "act_activeKilocalories",
+        "act_distance",
+
+        # respiration static
+        "resp_lowestRespirationValue",
+        "resp_highestRespirationValue",
+        "resp_avgSleepRespirationValue",
+
+         'sleep_day_ptc'
+
+    ]
+
+    X_list = []
+    y_list = []
+
+    for group, users in groups_dfs.items():
+        for user_id, df in users.items():
+
+            df_model = df.copy()
+
+            # tieni solo righe valide
+            df_model = df_model.dropna(subset=["label"])
+
+            X = df_model[FEATURES]
+            y = df_model["label"]
+
+            X_list.append(X)
+            y_list.append(y)
+
+    X_all = pd.concat(X_list, axis=0)
+    y_all = pd.concat(y_list, axis=0)
+
+    X_all = X_all.replace([np.inf, -np.inf], np.nan)
+
+    split_idx = int(len(X_all) * 0.7)
+
+
+
+    X_train = X_all.iloc[:split_idx]
+    X_val   = X_all.iloc[split_idx:]
+
+
+
+    y_train = y_all.iloc[:split_idx]
+    y_val   = y_all.iloc[split_idx:]
+
+    cfg = replace_keys(unflatten_dict(context.run_config))
+    params = cfg["params"]
+
+    bst = XGBRegressor(
+        params=params
+    )
+    global_model = bytearray(msg.content["arrays"]["0"].numpy().tobytes())
+    # Load global model into booster
+    bst.load_model(global_model)
+    # Run evaluation
+    auc = bst.score(
+        X_val,
+        y_val
     )
 
     # Construct and return reply Message
     metrics = {
-        "eval_loss": eval_loss,
-        "eval_acc": eval_acc,
-        "num-examples": len(valloader.dataset),
+        "auc": auc,
+        "num-examples": len(X_val),
+        'mae': mean_absolute_error(y_val, bst.predict(X_val)),
     }
     metric_record = MetricRecord(metrics)
     content = RecordDict({"metrics": metric_record})
     return Message(content=content, reply_to=msg)
+
+
+    # # Load model and data
+    # partition_id = context.node_config["partition-id"]
+    # num_partitions = context.node_config["num-partitions"]
+    # _, valid_dmatrix, _, num_val = load_data(partition_id, num_partitions)
+
+    # # Load config
+    # cfg = replace_keys(unflatten_dict(context.run_config))
+    # params = cfg["params"]
+
+    # # Load global model
+    # bst = xgb.Booster(params=params)
+    # global_model = bytearray(msg.content["arrays"]["0"].numpy().tobytes())
+    # bst.load_model(global_model)
+
+    # # Run evaluation
+    # eval_results = bst.eval_set(
+    #     evals=[(valid_dmatrix, "valid")],
+    #     iteration=bst.num_boosted_rounds() - 1,
+    # )
+    # auc = float(eval_results.split("\t")[1].split(":")[1])
+
+    # # Construct and return reply Message
+    # metrics = {
+    #     "auc": auc,
+    #     "num-examples": num_val,
+    # }
+    # metric_record = MetricRecord(metrics)
+    # content = RecordDict({"metrics": metric_record})
+    # return Message(content=content, reply_to=msg)
--- challengcsibalance/server/server_app.py
+++ challengcsibalance/server/server_app.py
@@ -1,48 +1,224 @@
-"""ChallengCSIBALANCE: A Flower / PyTorch app."""
+"""quickstart_xgboost: A Flower / XGBoost app."""
 
-import torch
-from flwr.app import ArrayRecord, ConfigRecord, Context
+import numpy as np
+import xgboost as xgb
+from flwr.app import ArrayRecord, Context
+from flwr.common.config import unflatten_dict
 from flwr.serverapp import Grid, ServerApp
-from flwr.serverapp.strategy import FedAvg
-
-from challengcsibalance.lib.task import Net
-from challengcsibalance.lib.data_handling import ensure_data, fetch_from_kaggle
-
-
-
+from flwr.serverapp.strategy import FedXgbBagging
+from challengcsibalance.lib.task import replace_keys
+from xgboost import XGBRegressor
 
 # Create ServerApp
 app = ServerApp()
 
 
-
 @app.main()
-def main(grid: Grid, context: Context) -> None:
-    """Main entry point for the ServerApp."""
-    # Ensure data is available
-    pass
+def main(grid: Grid, context: Context) -> None: 
+    print("Starting Flower XGBoost server...")
+    print(context.run_config)
+    # Read run config
+    num_rounds = context.run_config["num-server-rounds"]
+    fraction_train = context.run_config["fraction-train"]
+    fraction_evaluate = context.run_config["fraction-evaluate"]
+    # Flatted config dict and replace "-" with "_"
+    cfg = replace_keys(unflatten_dict(context.run_config))
+    params = cfg["params"]
 
-    # # Read run config
-    # fraction_train: float = context.run_config["fraction-train"]
-    # num_rounds: int = context.run_config["num-server-rounds"]
-    # lr: float = context.run_config["lr"]
+    # Init global model
+    # Init with an empty object; the XGBooster will be created
+    # and trained on the client side.
+    global_model = b""
+    # Note: we store the model as the first item in a list into ArrayRecord,
+    # which can be accessed using index ["0"].
+    arrays = ArrayRecord([np.frombuffer(global_model, dtype=np.uint8)])
 
-    # # Load global model
-    # global_model = Net()
-    # arrays = ArrayRecord(global_model.state_dict())
+    # Initialize FedXgbBagging strategy
+    strategy = FedXgbBagging(
+        fraction_train=fraction_train,
+        fraction_evaluate=fraction_evaluate,
+    )
 
-    # # Initialize FedAvg strategy
-    # strategy = FedAvg(fraction_train=fraction_train)
-
-    # # Start strategy, run FedAvg for `num_rounds`
-    # result = strategy.start(
-    #     grid=grid,
-    #     initial_arrays=arrays,
-    #     train_config=ConfigRecord({"lr": lr}),
-    #     num_rounds=num_rounds,
-    # )
+    # # Start strategy, run FedXgbBagging for `num_rounds`
+    result = strategy.start(
+        grid=grid,
+        initial_arrays=arrays,
+        num_rounds=num_rounds,
+    )
 
     # # Save final model to disk
-    # print("\nSaving final model to disk...")
-    # state_dict = result.arrays.to_torch_state_dict()
-    # torch.save(state_dict, "final_model.pt")
+    global_model = bytearray(result.arrays["0"].numpy().tobytes())
+
+    # Load aggregated model
+    booster = xgb.Booster()
+    booster.load_model(global_model)
+
+
+    # X_train, y_train, X_val, y_val = load_test_data()
+    # dtest=xgb.DMatrix(X_val, label=y_val)
+    # preds = booster.predict(dtest)
+    # from sklearn.metrics import mean_absolute_error
+    # mae = mean_absolute_error(y_val, preds)
+    # print(f"Final MAE on validation set: {mae}")
+
+    booster.save_model("final_model.json")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import pandas as pd
+import numpy as np
+import re
+from pathlib import Path
+import os
+import ast
+from challengcsibalance.lib.task import convert_timeseries_columns, clean_ts
+
+def load_test_data():
+    base = Path('./exploration/filtered_data/')
+
+    groups_dfs = {}
+
+
+    for csv_path in sorted(base.glob(f'group*/*.csv')):
+        group = csv_path.parent.name
+        m = re.search(r'dataset_user_(\d+)_train\.csv', csv_path.name)
+        if not m:
+            continue
+        user_id = int(m.group(1))
+        df = pd.read_csv(csv_path)
+        groups_dfs.setdefault(group, {})[user_id] = df
+
+
+    for group, user_dfs in groups_dfs.items():
+        for user_id, df in user_dfs.items():
+            groups_dfs[group][user_id] = convert_timeseries_columns(df)
+    
+    for group, user_dfs in groups_dfs.items():
+        for user_id, df in user_dfs.items():
+            for col in df.columns:
+                if col.startswith('timeseries_'):
+                    df[col] = df[col].apply(lambda x: clean_ts(x))
+            groups_dfs[group][user_id] = df
+    
+
+    for group, users in groups_dfs.items():
+        for user_id, df in users.items():
+            df['deep_sleep_pct'] = np.nan
+            df['rem_sleep_pct'] = np.nan
+            df['light_sleep_pct'] = np.nan
+            df['awake_sleep_pct'] = np.nan
+            df['sleep_efficiency'] = np.nan
+            df['sleep_day_ptc'] = np.nan
+            for row_idx, row in df.iterrows():
+                sleep_deepSleepSeconds = row.get("sleep_deepSleepSeconds", np.nan)
+                sleep_lightSleepSeconds = row.get("sleep_lightSleepSeconds", np.nan)
+                sleep_remSleepSeconds = row.get("sleep_remSleepSeconds", np.nan)
+                sleep_timeSeconds = row.get("sleep_sleepTimeSeconds", np.nan)
+                sleep_awakeTimeSeconds = row.get("sleep_awakeSleepSeconds", np.nan)
+                
+
+
+                deep_pct = sleep_deepSleepSeconds / sleep_timeSeconds 
+                rem_pct = sleep_remSleepSeconds / sleep_timeSeconds
+                light_pct = sleep_lightSleepSeconds / sleep_timeSeconds
+                awake_pct = sleep_awakeTimeSeconds / sleep_timeSeconds
+                sleep_efficiency = sleep_timeSeconds / (sleep_timeSeconds + sleep_awakeTimeSeconds)
+                df.at[row_idx, 'deep_sleep_pct'] = deep_pct
+                df.at[row_idx, 'rem_sleep_pct'] = rem_pct
+                df.at[row_idx, 'light_sleep_pct'] = light_pct
+                df.at[row_idx, 'awake_sleep_pct'] = awake_pct
+                df.at[row_idx, 'sleep_efficiency'] = sleep_efficiency
+                df.at[row_idx, 'sleep_day_ptc'] = sleep_timeSeconds / 86400.0
+            
+            groups_dfs[group][user_id] = df
+    FEATURES = [
+        # sleep composition
+        "deep_sleep_pct",
+        "rem_sleep_pct",
+        "light_sleep_pct",
+        "awake_sleep_pct",
+        "sleep_efficiency",
+
+        # heart rate static
+        "hr_restingHeartRate",
+        "hr_lastSevenDaysAvgRestingHeartRate",
+        "hr_maxHeartRate",
+        "hr_minHeartRate",
+
+        # stress static
+        "str_avgStressLevel",
+        "str_maxStressLevel",
+
+        # activity
+        "act_totalCalories",
+        "act_activeKilocalories",
+        "act_distance",
+
+        # respiration static
+        "resp_lowestRespirationValue",
+        "resp_highestRespirationValue",
+        "resp_avgSleepRespirationValue",
+
+        'sleep_day_ptc'
+    ]
+
+    X_list = []
+    y_list = []
+
+    for group, users in groups_dfs.items():
+        for user_id, df in users.items():
+
+            df_model = df.copy()
+
+            # tieni solo righe valide
+            df_model = df_model.dropna(subset=["label"])
+
+            X = df_model[FEATURES]
+            y = df_model["label"]
+
+            X_list.append(X)
+            y_list.append(y)
+
+    X_all = pd.concat(X_list, axis=0)
+    y_all = pd.concat(y_list, axis=0)
+
+    X_all = X_all.replace([np.inf, -np.inf], np.nan)
+
+    split_idx = int(len(X_all) * 0.7)
+
+
+
+    X_train = X_all.iloc[:split_idx]
+    X_val   = X_all.iloc[split_idx:]
+
+
+
+    y_train = y_all.iloc[:split_idx]
+    y_val   = y_all.iloc[split_idx:]
+    return X_train, y_train, X_val, y_val
